cafb8603eb6516eb3b91b716b2a7c4eb4267e79f
cburwell <burwell.cameron@gmail.com>
2015-11-01 11:59:12 -0800

    Added initial API modules and started creating Prototype 1. Need to get access to tri data, using run transition times for Matt's run for now.

--- StravAwesome.js
+++ StravAwesome.js
@@ -1,1 +1,31 @@
-/* test commit */
+var strava = require('strava-v3');
+
+/*var API = {
+    function partition(raceFile) {}
+
+    function getTransitionTime(raceFile) {}
+}*/
+
+//module.exports = API;
+
+//258550909 chick
+//258883512 dude
+//415868706 matt
+
+
+// velocity in m/s
+strava.streams.activity({ id:415868706, types:["velocity_smooth", "moving"]}, function(err, payload) {
+    if(!err) {
+        console.log(payload);
+    }
+    else {
+        console.log(err);
+    }
+});
+
+/**
+    distance: in meters
+    time: in seconds
+
+    strava.streams.activity
+*/
