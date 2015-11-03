f337808dc4bb770290a6a59b224f29dcd4982e90
ccsheeha <ccsheeha@calpoly.edu>
2015-11-03 11:47:58 -0800

    added this

---
+++ getMostLikelyActivity.js
@@ -0,0 +1,23 @@
+var strava = require('strava-v3');
+
+var mostLikelyActivity = {
+	/*
+	 * Activity objects require parameters:
+	 * name: 				string
+	 */
+	getMostLikelyActivity : function (velocity_smooth, moving) {
+
+	/*
+	* 	NOT YET IMPLEMENTED
+	*
+	*/
+
+
+
+	}
+
+
+
+
+}
+module.exports = getMostLikelyActivity;
